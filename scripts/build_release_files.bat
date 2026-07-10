a77fe21b51a0062086cbfe73553e1b9ec962e6c1
Andrey Putilov <Andrey.Putilov@materialplus.io>
2026-07-10 15:17:42 -0700

    Build step through pinliner, again here too

--- scripts/build_release_files.bat
+++ scripts/build_release_files.bat
@@ -13,19 +13,46 @@ CALL gen_ver_file.bat
 if %ERRORLEVEL% NEQ 0 ( echo ERROR: Failure && pause && exit /b %errorlevel% )
 
 
+@REM ECHO - Copy program files
+@REM COPY ..\src\setup.py ..\dist\
+@REM COPY ..\src\setup_files_data.py ..\dist\
+@REM MKDIR ..\dist\GENERATED
+@REM COPY ..\src\GENERATED\* ..\dist\GENERATED\
 
-COPY ..\src\setup.py ..\dist\
+PUSHD ..
+ECHO - Deliver program through pinliner
+ECHO Produce distributable .py bundle - calling pinliner...
+REM REM :: comment: please delete .pyc files before every call of the setup - this is implemented in my fork of the pinliner
+@REM python src_dev_build\lib\pinliner\pinliner\pinliner.py src -o dist\setup.py --verbose
+python src_dev_build\lib\pinliner\pinliner\pinliner.py src -o dist\setup.py
+if %ERRORLEVEL% NEQ 0 ( echo ERROR: Failure && pause && exit /b %errorlevel% )
+ECHO Done
+
+ECHO -
+ECHO -
+ECHO Patching setup.py...
+ECHO # ... >> dist\setup.py
+ECHO # print('within setup') >> dist\setup.py
+REM REM :: no need for this, the root package is loaded automatically
+@REM ECHO # import setup >> dist\setup.py
+ECHO from src import setup >> dist\setup.py
+ECHO setup.main() >> dist\setup.py
+ECHO # print('out of setup') >> dist\setup.py
+ECHO Done
+POPD
+
+
+ECHO - Copy all other files - README, LICENSE, etc
 COPY ..\README.md ..\dist\
 COPY ..\LICENSE ..\dist\
 ECHO # > ..\dist\requirements.txt
+
+ECHO - Copy scripts
 COPY .\setup.bat ..\dist\
 powershell -NoProfile -Command ^
   "(Get-Content '..\dist\setup.bat') -Replace 'PUSHD ..', 'PUSHD .' | Set-Content '..\dist\setup.bat'"
 COPY .\uninstall.bat ..\dist\
 powershell -NoProfile -Command ^
   "(Get-Content '..\dist\uninstall.bat') -Replace 'PUSHD ..', 'PUSHD .' | Set-Content '..\dist\uninstall.bat'"
-COPY ..\src\setup_files_data.py ..\dist\
-MKDIR ..\dist\GENERATED
-COPY ..\src\GENERATED\* ..\dist\GENERATED\
 
 
